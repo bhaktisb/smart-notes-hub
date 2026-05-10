e9b18ac5fa057e097b7ca46ecf5b0f834ea63e0d
bhaktisb <bhaktibhande1@gmail.com>
2026-05-11 00:22:00 +0530

    Fixed Render deployment

--- backend/app.py
+++ backend/app.py
@@ -52,5 +52,8 @@ def delete_note(id):
     })
 
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
